bf365f2a501cff7c79331ebf75b520c83b956295
pgomez87 <jpablo.gz.10@gmail.com>
2021-08-04 07:52:10 -0600

    Fetch data from data base & Error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productsRoutes from './routes/productsRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +16,12 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+// Every time we need to manage products
+app.use('/api/products', productsRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
