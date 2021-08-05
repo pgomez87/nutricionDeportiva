327d87770ab48cae4f9740878cf3fc4a58ed764a
pgomez87 <jpablo.gz.10@gmail.com>
2021-08-05 07:58:22 -0600

    Shopping Cart Funcionality: Screen interface, Add & remove items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
           {/* :id = param (We use it to match if needed in an conditional) */}
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
