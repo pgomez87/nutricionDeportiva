75e37cb33ba700af637c3fcd92bed4bf6d39e9a9
pgomez87 <jpablo.gz.10@gmail.com>
2021-08-05 23:04:05 -0600

    User Creation & password Encryption using JWT

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productsRoutes from './routes/productsRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,15 +13,17 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 // Every time we need to manage products
 app.use('/api/products', productsRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
